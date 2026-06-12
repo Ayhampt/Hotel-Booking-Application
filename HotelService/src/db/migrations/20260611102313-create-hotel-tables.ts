d085ae21e0b26941c0afcddd452982eb111c5afc
Ayham <ayhampt011@gmail.com>
2026-06-12 07:42:55 +0530

    Added sequelize config for migrations

---
+++ HotelService/src/db/migrations/20260611102313-create-hotel-tables.ts
@@ -0,0 +1,22 @@
+import { QueryInterface } from "sequelize";
+export default {
+  async up(queryInterface: QueryInterface) {
+    queryInterface.sequelize.query(`
+      CREATE TABLE IF NOT EXISTS hotels (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        address VARCHAR(255) NOT NULL,
+        location VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id)
+      );
+      `);
+  },
+
+  async down(queryInterface: QueryInterface) {
+    queryInterface.sequelize.query(`
+      DROP TABLE IF EXISTS hotels;
+      `);
+  },
+};
